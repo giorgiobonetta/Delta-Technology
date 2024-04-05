ba660b249ac01f7bca400800bbadac6023bd274f
giorgiobonetta <165094707+giorgiobonetta@users.noreply.github.com>
2024-04-05 17:22:18 +0200

    Update Simulating Geometric Brownian Motion.py

--- Simulating Geometric Brownian Motion.py
+++ Simulating Geometric Brownian Motion.py
@@ -1,17 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Simulating Geometric Brownian Motion
-
-# In[8]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# In[10]:
-
 
 mu= 0.1
 n= 100
@@ -19,10 +7,6 @@ T= 1
 M= 100
 S0=100
 sigma=0.3
-
-
-# In[18]:
-
 
 dt=T/n
 St=np.exp(
@@ -34,17 +18,9 @@ St = np.vstack([np.ones(M), St])
 
 St = S0*St.cumprod(axis=0)
 
-
-# In[32]:
-
-
 time=np.linspace(0,T,n+1)
 
 tt=np.full(shape=(M,n+1), fill_value=time).T
-
-
-# In[44]:
-
 
 plt.plot(tt, St)
 plt.xlabel('Years $(t)$')
@@ -52,10 +28,3 @@ plt.ylabel('Stock Price $(S_t)$')
 plt.title('Realizations of Geometric Brownian Motion\n $dS_t = \mu S_t dt + \sigma S_t dW_t$\n')
          
 plt.show()
-
-
-# In[ ]:
-
-
-
-
